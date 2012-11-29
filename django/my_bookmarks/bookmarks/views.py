ff5bca1aa58e8e3fc5856999bdeb03f04935244e
nostradamnit <me@nostradamnit.com>
2012-11-29 08:29:26 +0100

    cleaned up views, consolidated upvote/downvote into one function

--- django/my_bookmarks/bookmarks/views.py
+++ django/my_bookmarks/bookmarks/views.py
@@ -11,7 +11,22 @@ from datetime import datetime
 
 def index(request):
     bookmarks = Bookmark.objects.filter(pub_date__lte=datetime.utcnow).order_by('-pub_date')[:10]
-    return render_to_response('index.html', {'bookmarks': bookmarks}, context_instance=RequestContext(request))
+    return render_to_response('index.html', 
+                                {'bookmarks': bookmarks}, 
+                                context_instance=RequestContext(request))
+
+def details(request, bookmark_id):
+    bookmark = get_object_or_404(Bookmark, pk=bookmark_id)
+    return render_to_response('bookmark_details.html', 
+                                {'bookmark': bookmark}, 
+                                context_instance=RequestContext(request))
+
+@login_required
+def show_user_bookmarks(request):
+    bookmarks = Bookmark.objects.filter(author=request.user)
+    return render_to_response('show_user_bookmarks.html', 
+                                {'bookmarks': bookmarks}, 
+                                context_instance=RequestContext(request))
 
 @login_required
 def add_bookmark(request):
@@ -21,50 +36,33 @@ def add_bookmark(request):
             bookmark = form.save(commit=False)
             bookmark.author = request.user
             bookmark.save()
-            return render_to_response('add_bookmark_thanks.html', context_instance=RequestContext(request))
+            return render_to_response('add_bookmark_thanks.html', 
+                                        context_instance=RequestContext(request))
     else:
         form = BookmarkForm()
         
-    return render_to_response('add_bookmark.html', {'form':form}, context_instance=RequestContext(request))
-    
-@login_required
-def show_user_bookmarks(request):
-    bookmarks = Bookmark.objects.filter(author=request.user)
-    return render_to_response('show_user_bookmarks.html', {'bookmarks': bookmarks}, context_instance=RequestContext(request))
+    return render_to_response('add_bookmark.html', 
+                                {'form':form}, 
+                                context_instance=RequestContext(request))
+
 
 @login_required
-def upvote(request, bookmark_id):
-    bookmark = Bookmark.objects.get(pk=bookmark_id)
-    bookmark.upvote()
+def vote(request, bookmark_id, direction):
+    bookmark = get_object_or_404(Bookmark, pk=bookmark_id)
+    getattr(bookmark, direction)() # dynamically call the right direction
     bookmark.save()
     result = bookmark.votes
     if request.is_ajax():
         json_data = json.dumps({'votes' : result })
         return HttpResponse(json_data, mimetype='application/json')
     else:
-        return render_to_response('vote_result.html', {'result': result }, context_instance=RequestContext(request))
-
-@login_required
-def downvote(request, bookmark_id):
-    bookmark = Bookmark.objects.get(pk=bookmark_id)
-    bookmark.downvote()
-    bookmark.save()
-    result = bookmark.votes
-    if request.is_ajax():
-        json_data = json.dumps({'votes' : result })
-        return HttpResponse(json_data, mimetype='application/json')
-    else:
-        return render_to_response('vote_result.html', {'result': result }, context_instance=RequestContext(request))
-
-
-
-def details(request, bookmark_id):
-    bookmark = Bookmark.objects.get(pk=bookmark_id)
-    return render_to_response('bookmark_details.html', {'bookmark': bookmark}, context_instance=RequestContext(request))
+        return render_to_response('vote_result.html', 
+                                    {'result': result }, 
+                                    context_instance=RequestContext(request))
 
 @login_required
 def modify(request, bookmark_id):
-    bookmark = Bookmark.objects.get(pk=bookmark_id, author=request.user)
+    bookmark = get_object_or_404(Bookmark, pk=bookmark_id, author=request.user)
     if request.method == 'POST':
         form = BookmarkForm(request.POST, instance=bookmark)
         if form.is_valid():
@@ -74,14 +72,18 @@ def modify(request, bookmark_id):
             messages.success(request, 'Bookmark details updated.')
             return HttpResponseRedirect('/bookmark/%s/'%bookmark_id)
         else:
-            return render_to_response('bookmark_modify.html', {'bookmark_id': bookmark_id, 'form': form}, context_instance=RequestContext(request))
+            return render_to_response('bookmark_modify.html', 
+                                        {'bookmark_id': bookmark_id, 'form': form}, 
+                                        context_instance=RequestContext(request))
     else:
         form = BookmarkForm(instance=bookmark)
-        return render_to_response('bookmark_modify.html', {'bookmark_id': bookmark_id, 'form': form}, context_instance=RequestContext(request))
+        return render_to_response('bookmark_modify.html', 
+                                    {'bookmark_id': bookmark_id, 'form': form}, 
+                                    context_instance=RequestContext(request))
 
 @login_required
 def delete(request, bookmark_id):
-    bookmark = Bookmark.objects.get(pk=bookmark_id, author=request.user)
+    bookmark = get_object_or_404(Bookmark, pk=bookmark_id, author=request.user)
     bookmark.delete()
     messages.success(request, 'Bookmark successfully deleted.')
     return HttpResponseRedirect('/bookmark/show_mine/')
